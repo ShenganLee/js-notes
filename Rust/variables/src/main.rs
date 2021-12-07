da24df85cb4cc22900ca9b7279b64717aff973bf
twist <lsa1669778762@gmail.com>
2021-12-07 19:44:06 +0800

    rest

---
+++ Rust/variables/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    // let x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+
+
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+
+
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    // let y = 7;
+    // println!("The value of y is: {}", y);
+
+    // let y = 8;
+    // println!("The value of y is: {}", y);
+
+
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    // let y = x;
+    // println!("The value of y is: {}", y);
+
+    // x = 7;
+    // println!("The value of x, y is: {}, {}", x, y);
+
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    println!("The value of THREE_HOURS_IN_SECONDS is: {}", THREE_HOURS_IN_SECONDS);
+}
